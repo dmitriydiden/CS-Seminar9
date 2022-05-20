e0db47f9aee44f02da24899aa9033bf047830faa
dmitry_di <dnd_d@mail.ru>
2022-05-20 18:20:29 +0300

    С помощью рекурсии написали программу подсчета суммы цифр в заданном числе

---
+++ task67/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 67:** Напишите программу, которая будет 
+принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+using System;
+using static System.Console;
+Clear();
+Write("Введите число М - ");
+int M=int.Parse(ReadLine());
+
+int sum = 0;
+SumNumber(M);
+WriteLine(sum);
+
+int SumNumber(int num)
+{
+    if (num == 0) return sum;
+    else
+    {
+        sum += num % 10;
+        return SumNumber(num/=10);
+    }
+}
