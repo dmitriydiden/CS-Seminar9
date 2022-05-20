0d3280e2915a3fe975a8d86e1fd4e278dded30dc
dmitry_di <dnd_d@mail.ru>
2022-05-20 19:04:31 +0300

    С помощью рекурсии написали программу подсчета суммы цифр в заданном числе

--- task67/Program.cs
+++ task67/Program.cs
@@ -9,16 +9,14 @@ Clear();
 Write("Введите число М - ");
 int M=int.Parse(ReadLine());
 
-int sum = 0;
-SumNumber(M);
+int sum = SumNumber(M);
 WriteLine(sum);
 
-int SumNumber(int num)
+static int SumNumber(int num)
 {
-    if (num == 0) return sum;
+    if (num == 0) return 0;
     else
     {
-        sum += num % 10;
-        return SumNumber(num/=10);
+       return num%10+SumNumber(num/=10);
     }
 }
