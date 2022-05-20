44adb412a91ce143603fdc930e793d05bb27a7de
dmitry_di <dnd_d@mail.ru>
2022-05-20 18:25:03 +0300

    С помощью рекурсии написали программу, которая выводит все натуральные числа в диапозоне от 1 до N

---
+++ task63/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 63:** Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+*/
+
+using System;
+using static System.Console;
+Clear();
+
+int N=5;
+string result=GetNumbers(1,N);
+WriteLine(result);
+
+
+
+static string GetNumbers(int start, int end)
+{
+    if (start==end) return start.ToString();
+    return(start+" "+GetNumbers(start+1, end));
+
+}
