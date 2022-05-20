b72187d8c5594068adb6849e247808ac66108a38
dmitry_di <dnd_d@mail.ru>
2022-05-20 19:14:35 +0300

    С помощью рекурсии написали программу, которая возводит числоА в степень В

---
+++ task69/Program.cs
@@ -0,0 +1,21 @@
+﻿/* **Задача 69:** Напишите программу, которая на вход 
+принимает два числа A и B, и возводит число А в целую
+степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+*/
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите число A - ");
+int A=int.Parse(ReadLine());
+Write("Введите число B - ");
+int B=int.Parse(ReadLine());
+
+WriteLine(degreeA(A,B));
+
+int degreeA (int a, int b)
+{
+    if (b==0) return 1;
+    return a*degreeA(a,--b);
+} 
